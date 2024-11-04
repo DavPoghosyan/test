90e3f0c6a85872b1b1f67f74a74a22c88821712c
davit.poghosyan <davit.poghosyan@webbfontaine.com>
2024-11-04 16:24:44 +0600

    Changing command line printout

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Bangladesh!");
+        System.out.println("Hello, Bangladesh!, From Armenia");
     }
 }
